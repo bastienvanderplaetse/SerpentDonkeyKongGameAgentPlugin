9a38db725c7a3959e5b1c0007377d960dc6261df
Bastien Vanderplaetse <bast.vdp@hotmail.com>
2018-05-05 18:14:52 +0200

    Fix out of memory error

--- files/serpent_DonkeyKong_game_agent.py
+++ files/serpent_DonkeyKong_game_agent.py
@@ -1,5 +1,6 @@
 from serpent.game_agent import GameAgent
 from serpent.input_controller import KeyboardKey
+import sys
 
 
 class SerpentDonkeyKongGameAgent(GameAgent):
@@ -14,7 +15,7 @@ class SerpentDonkeyKongGameAgent(GameAgent):
         self.analytics_client = None
 
         # Genetic algorithms parameters
-        self.population = 10
+        self.population = 4
         self.evaluated_individuals = 0
 
     def setup_play(self):
@@ -22,51 +23,32 @@ class SerpentDonkeyKongGameAgent(GameAgent):
 
     def handle_play(self, game_frame):
         if (self.game.api.not_running()):
-        	print("not running")
-        	self.game.api.run()
+            print("not running")
+            self.game.api.run()
         else:
-        	if (self.evaluated_individuals >= self.population):
-        		self.visual_debugger.store_image_data(
-                    game_frame.frame,
-                    game_frame.frame.shape,
-                    str(0)
-                )
-        		self.game.api.analyze_frame(game_frame)
-        		if (self.evaluated_individuals % 3 == 0):
-        			print("end generation")
-        		elif (self.game.api.is_dead()):
-        			print("Fake individual")
-        			self.evaluated_individuals = self.evaluated_individuals + 1
-        			self.game.api.replay()
-        	else:
-        		self.game.api.analyze_frame(game_frame)
-        		if (self.game.api.is_in_menu()):
-        			self.visual_debugger.store_image_data(
-                    	game_frame.frame,
-                    	game_frame.frame.shape,
-                    	str(1)
-                	)
-        			frame, units = self.game.api.get_mario_frame(game_frame)
-        			self.game.api.next()
-        			if (frame != None):
-        				print("DEMO")
-        				self.input_controller.tap_key(KeyboardKey.KEY_SPACE)
-        			self.input_controller.tap_key(KeyboardKey.KEY_ENTER)
-        			self.input_controller.tap_key(KeyboardKey.KEY_SPACE)
-        			self.input_controller.tap_key(KeyboardKey.KEY_ENTER)
-        		elif (self.game.api.is_playing()):
-        			self.visual_debugger.store_image_data(
-                    	game_frame.frame,
-                    	game_frame.frame.shape,
-                    	str(2)
-                	)
-        			print("playing")
-        		elif(self.game.api.is_dead()):
-        			self.visual_debugger.store_image_data(
-                    	game_frame.frame,
-                    	game_frame.frame.shape,
-                    	str(3)
-                	)
-        			print("Evaluate")
-        			self.evaluated_individuals = self.evaluated_individuals + 1
-        			self.game.api.replay()
+            if (self.evaluated_individuals >= self.population):
+                self.game.api.analyze_frame(game_frame)
+                if (self.evaluated_individuals % 3 == 0):
+                    print("end generation")
+                    sys.exit()
+                elif (self.game.api.is_dead()):
+                    print("Fake individual")
+                    self.evaluated_individuals = self.evaluated_individuals + 1
+                    self.game.api.replay()
+            else:
+                self.game.api.analyze_frame(game_frame)
+                if (self.game.api.is_in_menu()):
+                    frame, units = self.game.api.get_mario_frame(game_frame)
+                    self.game.api.next()
+                    if (frame != None):
+                        print("DEMO")
+                        self.input_controller.tap_key(KeyboardKey.KEY_SPACE)
+                    self.input_controller.tap_key(KeyboardKey.KEY_ENTER)
+                    self.input_controller.tap_key(KeyboardKey.KEY_SPACE)
+                    self.input_controller.tap_key(KeyboardKey.KEY_ENTER)
+                elif (self.game.api.is_playing()):
+                    print("playing")
+                elif(self.game.api.is_dead()):
+                    print("Evaluate")
+                    self.evaluated_individuals = self.evaluated_individuals + 1
+                    self.game.api.replay()
